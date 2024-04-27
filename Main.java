7b18a4386f3d155ab33bc6ec208cceea109f1ec4
Denis Shestakov <azia1864@gmail.com>
2024-04-27 16:47:53 +0200

    add Main update

--- Main.java
+++ Main.java
@@ -1,88 +1,97 @@
+
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        LaptopStore store = new LaptopStore();
-        Map<String, Object> filters = new HashMap<>(); // Изменили тип данных на Object
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Параметры ноутбуков:");
-        System.out.println("1 - Производитель");
-        System.out.println("2 - Операционная система");
-        System.out.println("3 - ОЗУ");
-        System.out.println("4 - Объем жесткого диска");
-        System.out.println("5 - Цвет");
-        System.out.println("Введите число для выбора параметра: ");
+        while (true) {
+            LaptopStore store = new LaptopStore();
+            Map<String, Object> filters = new HashMap<>();
 
-        int choice = scanner.nextInt();
-        scanner.nextLine(); // consume newline
-        
+            System.out.println("Параметры ноутбуков:");
+            System.out.println("1 - Производитель");
+            System.out.println("2 - Операционная система");
+            System.out.println("3 - ОЗУ");
+            System.out.println("4 - Объем жесткого диска");
+            System.out.println("5 - Цвет");
+            System.out.println("Введите число для выбора параметра (или 0 для выхода): ");
 
-        switch (choice) {
-            case 1:
-                System.out.println("Выберите производителя:");
-                for (String manufacturer : store.getAllManufacturers()) {
-                    System.out.println(manufacturer);   
-                }
-                System.out.println("Выберите производителя: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // consume newline
 
-                String selectedManufacturer = scanner.nextLine();
-                filters.put("Manufacturer", selectedManufacturer);
+            if (choice == 0) {
+                System.out.println("Программа завершена.");
                 break;
-            case 2:
-                System.out.println("Операционная система:");
-                for (String os : store.getAllOss()) {
-                    System.out.println(os);   
-                }
-                System.out.println("Выберите операционную систему: ");
+            }
 
-                String selectedOs = scanner.nextLine();
-                filters.put("Operating System", selectedOs);
-                break;
-            case 3:
-                System.out.println("ОЗУ:");
-                for (Integer ram : store.getAllRams()) {
-                    System.out.println(ram);   
-                }
-                System.out.println("Выберите ОЗУ: ");
+            if (choice < 1 || choice > 5) {
+                System.out.println("Недопустимое значение. Пожалуйста, введите число из меню.");
+                System.out.println();
+                continue;
+            }
 
-                int selectedRams = scanner.nextInt();
-                scanner.nextLine();
-                filters.put("RAM", selectedRams); // Теперь используется целочисленное значение
-                break;
-            case 4:
-                System.out.println("Объем жестекого диска:");
-                for (Integer storage : store.getAllStorages()) {
-                    System.out.println(storage);   
-                }
-                System.out.println("Выберите объем диска: ");
+            switch (choice) {
+                case 1:
+                    System.out.println("Выберите производителя:");
+                    for (String manufacturer : store.getAllManufacturers()) {
+                        System.out.println(manufacturer);
+                    }
+                    System.out.println("Выберите производителя: ");
 
-                int selectedStorages = scanner.nextInt();
-                scanner.nextLine();
-                filters.put("Storage", selectedStorages); // Теперь используется целочисленное значение
-                break; 
-            case 5:
-                System.out.println("Цвет:");
-                for (String color : store.getAllColors()) {
-                    System.out.println(color);   
-                }
-                System.out.println("Выберите цвет: ");
+                    String selectedManufacturer = scanner.nextLine();
+                    filters.put("Manufacturer", selectedManufacturer);
+                    break;
+                case 2:
+                    System.out.println("Операционная система:");
+                    for (String os : store.getAllOss()) {
+                        System.out.println(os);
+                    }
+                    System.out.println("Выберите операционную систему: ");
 
-                String selectedColors = scanner.nextLine();
-                //scanner.nextLine();
-                filters.put("Color", selectedColors); // Теперь используется целочисленное значение
-                break;      
-             
-                // Можете добавить аналогичные методы для остальных фильтров
-                // break;
-            default:
-                System.out.println("Недопустимый выбор");
-                System.out.println("Запустите программу снова");
-        }
+                    String selectedOs = scanner.nextLine();
+                    filters.put("Operating System", selectedOs);
+                    break;
+                case 3:
+                    System.out.println("ОЗУ:");
+                    for (Integer ram : store.getAllRams()) {
+                        System.out.println(ram);
+                    }
+                    System.out.println("Выберите ОЗУ: ");
 
-        Set<Laptop> filteredLaptops = store.filter(filters);
-        System.out.println("Отфильтрованные ноутбуки:");
-        for (Laptop laptop : filteredLaptops) {
-            System.out.println(laptop.getManufacturer() + " " + laptop.getOs() + " " + laptop.getRam() + "GB RAM " + laptop.getStorage() + "GB storage " + laptop.getColor());
+                    int selectedRams = scanner.nextInt();
+                    scanner.nextLine();
+                    filters.put("RAM", selectedRams); 
+                    break;
+                case 4:
+                    System.out.println("Объем жесткого диска:");
+                    for (Integer storage : store.getAllStorages()) {
+                        System.out.println(storage);
+                    }
+                    System.out.println("Выберите объем диска: ");
+
+                    int selectedStorages = scanner.nextInt();
+                    scanner.nextLine();
+                    filters.put("Storage", selectedStorages); 
+                    break;
+                case 5:
+                    System.out.println("Цвет:");
+                    for (String color : store.getAllColors()) {
+                        System.out.println(color);
+                    }
+                    System.out.println("Выберите цвет: ");
+
+                    String selectedColors = scanner.nextLine();
+                    filters.put("Color", selectedColors); 
+                    break;
+            }
+
+            Set<Laptop> filteredLaptops = store.filter(filters);
+            System.out.println("Отфильтрованные ноутбуки:");
+            for (Laptop laptop : filteredLaptops) {
+                System.out.println(laptop.getManufacturer() + " " + laptop.getOs() + " " + laptop.getRam() + "GB RAM " + laptop.getStorage() + "GB storage " + laptop.getColor());
+            }
+            // Остановка программы после вывода отфильтрованных ноутбуков
+            break;
         }
     }
 }
